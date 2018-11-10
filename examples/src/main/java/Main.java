14e1f3fc6be28f53b751149f0aa0608a862f4a79
ShellAddicted <ShellAddicted@gmail.com>
2018-11-10 23:40:18 +0100

    Update example (get token from env, and getFile() example)

--- examples/src/main/java/Main.java
+++ examples/src/main/java/Main.java
@@ -1,33 +1,31 @@
 import java.util.concurrent.ExecutionException;
 
 import im.dlg.botsdk.Bot;
+import im.dlg.botsdk.domain.content.Content;
 import im.dlg.botsdk.domain.content.DocumentContent;
 import im.dlg.botsdk.domain.content.TextContent;
-import im.dlg.botsdk.domain.interactive.InteractiveGroup;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-
-        Bot bot = Bot.start("cbb4994cabfa8d2a5bce0b5f7a44c23da943f767").get();
+        //
+        Bot bot = Bot.start(System.getenv("DIALOGS_BOT_TOKEN")).get();
 
         bot.messaging().onMessage(message ->
                 bot.users().get(message.getSender()).thenAccept(userOpt -> userOpt.ifPresent(user -> {
-                            System.out.println("Got a message: " + message.getText() + " from user: " + user.getName());
-                        })
-                ).thenCompose(aVoid -> {
-                            if (message.getText().equals("Send me photo")) {
-                                bot.messaging().sendMedia(message.getPeer(), ((TextContent) message.getMessageContent()).getMedias());
-                            } else if (message.getText().equals("Send me video")) {
-                                bot.messaging().sendDocument(message.getPeer(), (DocumentContent) message.getMessageContent());
-                            } else {
-//                                bot.interactiveApi().send(message.getPeer())
-//                                bot.interactiveApi().send(message.getPeer(), new InteractiveGroup())
+                            System.out.println("Got a message from user: " + user.getName() + " " + message.getMessageContent().toString());
+                            Content cnt = message.getMessageContent();
+                            if (cnt instanceof DocumentContent) {
+                                bot.messaging().sendText(message.getPeer(), "File Received.");
+                                bot.messaging().getFileUrl(((DocumentContent) message.getMessageContent()).getFileId(), ((DocumentContent) message.getMessageContent()).getAccessHash()).thenAccept(res -> {
+                                    System.out.println("Received a file: " + res.getUrl() + " (timeout: " + res.getTimeout() + ") ");
+                                });
+                            } else if (cnt instanceof TextContent) {
+                                System.out.println("Received Text: " + ((TextContent) message.getMessageContent()).getText());
+                                bot.messaging().sendText(message.getPeer(), "Echo: " + ((TextContent) message.getMessageContent()).getText());
                             }
-                            return null;
-                        }
-                ).thenAccept(uuid ->
-                        System.out.println("Sent a message with UUID: " + uuid)));
+                        })
+                ));
 
         bot.await();
     }
